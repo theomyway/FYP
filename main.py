c4de2f55d736c2aabb5b29b08191ae66a99d5411
Omar <Omar@OMARSPC>
2023-04-25 13:45:43 +0500

    1 - I messed the project up.. finally recovered. 2 - Added labels of covid and non covid for CNN model in form of npy arrays 3 - Edited its code. Now npy arrays load indivisually. Done that as Github cannot accept files more than 100mb at a time. 4- Deploying on heroku changes main code, Added port 5000. 5 - Added Procfile and deployment mechanism and edited requirements file. 6 - Upgraded numpy 7 - Upgraded tensorflow lib

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+from flask import Flask
+
 from website import create_app
 import os
 
-app = create_app()
+app = Flask(__name__)
+
+# Set the FLASK_APP environment variable
+os.environ['FLASK_APP'] = 'main.py'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000),debug=True)
+    app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
