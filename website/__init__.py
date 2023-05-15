7f3af335f3962c983272af12da023dfbe0c917d9
Omar <Omar@OMARSPC>
2023-05-16 00:51:49 +0500

    2 - Added labels of covid and non covid for CNN model in form of npy arrays 3 - Edited its code. Now npy arrays load indivisually. Done that as Github cannot accept files more than 100mb at a time. 4- Deploying on heroku changes main code, Added port 5000. 5 - Added Procfile and deployment mechanism and edited requirements file. 6 - Upgraded numpy 7 - Upgraded tensorflow lib 8 - Only X-ray image will be accepted by the app. 9 - Some UI/Ux changes 10- Will not accept JFIF file now. Only .png .jpg and .jpeg exts allowed! 11- K value changed to 32 for KNN for better results. 12- Set up venv and installed necessary libraries.

--- website/__init__.py
+++ website/__init__.py
@@ -7,12 +7,11 @@ db = SQLAlchemy()
 DB_NAME = "database.db"
 
 
-
-
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'theomyway'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
     from .views import views
@@ -21,7 +20,7 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User, Image ,Note
+    from .models import User, Image, Note
 
     with app.app_context():
         db.create_all()
